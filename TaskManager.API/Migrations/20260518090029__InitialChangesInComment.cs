997805ff64bd848c62bfee9cf1fe8d72ff0e360d
Sambidhan26 <sambidhan26@gmail.com>
2026-05-18 17:49:49 +0800

    database updated for Comment model, Comment Dtos and CRUD implementation done and working well

---
+++ TaskManager.API/Migrations/20260518090029__InitialChangesInComment.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace TaskManager.API.Migrations
+{
+    /// <inheritdoc />
+    public partial class _InitialChangesInComment : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<int>(
+                name: "TaskItemId",
+                table: "Comments",
+                type: "int",
+                nullable: true,
+                oldClrType: typeof(int),
+                oldType: "int");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<int>(
+                name: "TaskItemId",
+                table: "Comments",
+                type: "int",
+                nullable: false,
+                defaultValue: 0,
+                oldClrType: typeof(int),
+                oldType: "int",
+                oldNullable: true);
+        }
+    }
+}
